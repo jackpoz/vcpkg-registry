1311d9c878a0dcf9195b3190dfe112a7f03f137c
Giacomo Pozzoni <giacomopoz@gmail.com>
2025-09-16 23:56:49 +0200

    Add fork of arcdps-unofficial-extras

---
+++ ports/arcdps-unofficial-extras/portfile.cmake
@@ -0,0 +1,29 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO jackpoz/arcdps_unofficial_extras_releases
+    REF ab7fc8915c054257edefc30c95657d77ecfcbe41
+    SHA512 113dab74ca713eec7d00b8ce9005c2443fa5bbfd736c9a90a02e5c9b06f185307e9b27705f484b129e1976923afc74d390e3e242bfab70e53b855802b6a3d1f0
+    HEAD_REF features/asan
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    magic-enum				ARCPDS_UNOFFICIAL_EXTRAS_MAGIC_ENUM
+    cpp11					ARCPDS_UNOFFICIAL_EXTRAS_CPP_11
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME ArcdpsUnofficialExtras)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
