66d5103d994100eea74444191590f01440fb0858
Giacomo Pozzoni <giacomopoz@gmail.com>
2025-09-18 00:03:53 +0200

    Update registry

--- ports/arcdps-unofficial-extras/portfile.cmake
+++ ports/arcdps-unofficial-extras/portfile.cmake
@@ -3,9 +3,9 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jackpoz/arcdps_unofficial_extras_releases
-    REF ab7fc8915c054257edefc30c95657d77ecfcbe41
-    SHA512 05ababe3eb27ea4ff5a19e405ee5081555493dae3cc10c2303500afd715c437c782c5d52ca0b3e402e4fe1514e7d136b7cd5dc54917ceb372137e2eb95c324e5
-    HEAD_REF features/asan
+    REF 7cf97fcd17df36b2fd9c57114e1333c190ada3ee
+    SHA512 929d25edab56ec840be94dd5a1a8769c572983780ea7320100695be554bf619cd7ffeeca9124a889808278350a30d00439f01e401d474b21acb94cbad476159c
+    HEAD_REF features/asan-wine
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
